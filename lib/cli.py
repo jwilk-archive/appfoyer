56ba2aabcc624f05624ecd9f62da378bbf933ddf
Jakub Wilk <jwilk@jwilk.net>
2018-05-28 10:54:27 +0200

    Port from Travis CI to Appveyor.

--- lib/cli.py
+++ lib/cli.py
@@ -6,20 +6,17 @@ command-line interface
 '''
 
 import argparse
-import collections
 import io
 import json
 import re
-import shutil
 import subprocess
-import sys
 import urllib.parse
 import urllib.request
 
 import lib.colors
 import lib.pager
 
-user_agent = 'trava (https://github.com/jwilk/trava)'
+user_agent = 'appfoyer (https://github.com/jwilk/appfoyer)'
 
 def get(url, headers=()):
     headers = dict(headers)
@@ -32,7 +29,7 @@ def get(url, headers=()):
 def get_json(url, headers=()):
     headers = dict(headers)
     headers.update(
-        {'Content-Type': 'application/vnd.travis-ci.2+json'}
+        {'Content-Type': 'application/json'}
     )
     with get(url, headers) as fp:
         with io.TextIOWrapper(fp, encoding='UTF-8') as tfp:
@@ -58,26 +55,26 @@ def get_git_url():
     if netloc == 'github.com':
         if path.endswith('.git'):
             path = path[:-4]
+        path = path.lstrip('/')
         return path
     else:
         return
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--raw-cr', action='store_true')
     ap.add_argument('url', metavar='URL')
     options = ap.parse_args()
     if options.url == '.':
         options.url = get_git_url()
-    url = urllib.parse.urljoin('https://travis-ci.org/', options.url)
+    url = urllib.parse.urljoin('https://ci.appveyor.com/project/', options.url)
     (scheme, netloc, path, query, fragment) = urllib.parse.urlsplit(url)
     if scheme not in {'http', 'https'}:
         ap.error('unsupported URL')
-    if netloc != 'travis-ci.org':
+    if netloc != 'ci.appveyor.com':
         ap.error('unsupported URL')
     for regex, cmd in _dispatch:
         regex = ('/' if regex else '') + regex
-        regex = r'\A/(?P<project>[\w.-]+/[\w.-]+){re}\Z'.format(re=regex)
+        regex = r'\A/project/(?P<project>[\w.-]+/[\w.-]+){re}\Z'.format(re=regex)
         match = re.match(regex, path)
         if match is not None:
             break
@@ -87,28 +84,26 @@ def main():
         return cmd(options, **match.groupdict())
 
 @dispatch('')
-@dispatch('branches')
-def show_branches(options, project):
-    url = 'https://api.travis-ci.org/repos/{project}/branches'
+@dispatch('history')
+def show_history(options, project):
+    url = 'https://ci.appveyor.com/api/projects/{project}/history?recordsNumber=10'
     url = url.format(project=project)
     data = get_json(url)
-    commits = {c['id']: c for c in data['commits']}
-    for branch in data['branches']:
-        commit = commits[branch['commit_id']]
-        template = '#{number} {branch} {state}'
+    for build in data['builds']:
+        template = '{version} {branch} {status}'
         curious = False
-        if branch['finished_at'] is None:
+        if build['finished'] is None:
             template = '{t.yellow}' + template
-        elif branch['state'] != 'passed':
+        elif build['status'] != 'success':
             template = '{t.bold}{t.red}' + template
             curious = True
         lib.colors.print(template,
-            number=branch['number'],
-            branch=commit['branch'],
-            state=branch['state'],
+            version=build['version'],
+            branch=build['branch'],
+            status=build['status'],
         )
-        url = 'https://travis-ci.org/{project}/builds/{id}'
-        url = url.format(project=project, id=branch['id'])
+        url = 'https://ci.appveyor.com/project/{project}/build/{version}'
+        url = url.format(project=project, version=build['version'])
         template = '{t.cyan}'
         if curious:
             template += '{t.bold}'
@@ -116,39 +111,24 @@ def show_branches(options, project):
         lib.colors.print(template, url=url, space='')
         print()
 
-@dispatch(r'builds/(?P<build_id>\d+)')
-def show_build(options, project, build_id):
-    url = 'https://api.travis-ci.org/repos/{project}/builds/{id}'
-    url = url.format(project=project, id=build_id)
+@dispatch(r'build/(?P<version>[^/\s]+)')
+def show_build(options, project, version):
+    url = 'https://ci.appveyor.com/api/projects/{project}/build/{version}'
+    url = url.format(project=project, version=version)
     data = get_json(url)
-    config_coll = collections.defaultdict(set)
-    for job in data['matrix']:
-        for key, value in job['config'].items():
-            if isinstance(value, (dict, list)):
-                continue
-            config_coll[key].add(value)
-    for job in data['matrix']:
-        template = '#{number} {config}'
+    data = data['build']['jobs']
+    for job in data:
+        template = '{version} {name}'
         error = False
-        if job['finished_at'] is None:
+        if job['finished'] is None:
             template = '{t.yellow}' + template
-        elif job['result'] != 0:
+        elif job['status'] != 'success':
             error = True
             template = '{t.bold}{t.red}' + template
         template += '{t.off}'
-        config = []
-        for key, value in sorted(job['config'].items()):
-            if key.startswith('.'):
-                continue
-            if isinstance(value, (dict, list)):
-                continue
-            if len(config_coll[key]) == 1:
-                continue
-            config += ['{key}={value}'.format(key=key, value=value)]
-        config = ' '.join(config)
-        lib.colors.print(template, number=job['number'], config=config)
-        url = 'https://travis-ci.org/{project}/jobs/{id}'
-        url = url.format(project=project, id=job['id'])
+        lib.colors.print(template, version=version, name=job['name'])
+        url = 'https://ci.appveyor.com/project/{project}/build/{version}/job/{id}'
+        url = url.format(project=project, version=version, id=job['jobId'])
         template = '{t.cyan}'
         if error:
             template += '{t.bold}'
@@ -156,18 +136,38 @@ def show_build(options, project, build_id):
         lib.colors.print(template, url=url, space='')
         print()
 
-@dispatch(r'jobs/(?P<job_id>\d+)')
-def show_job(options, project, job_id):
-    url = 'https://api.travis-ci.org/jobs/{id}/log.txt'
+@dispatch(r'build/(?P<version>[^/\s]+)/job/(?P<job_id>\w+)')
+def show_job(options, project, version, job_id):
+    url = 'https://ci.appveyor.com/api/buildjobs/{id}/console'.format(id=job_id)
     url = url.format(id=job_id)
     with get(url) as fp:
-        if options.raw_cr:
-            shutil.copyfileobj(fp, sys.stdout.buffer)
-        else:
-            for line in fp:
-                line = line.rstrip(b'\r\n').rsplit(b'\r', 1)[-1]
-                sys.stdout.buffer.write(line)
-                sys.stdout.buffer.write(b'\n')
+        with io.TextIOWrapper(fp, encoding='UTF-8') as tfp:
+            data = tfp.read()
+    data = '[' + data[:-1] + ']'
+    data = json.loads(data)
+    template = '{t.dim}{time:8}{t.off} {text}'
+    last_ts = ''
+    text = ''
+    for item in data:
+        chunks = item['t']
+        chunks = chunks.replace('\r\n', '\n')
+        chunks = chunks.splitlines(keepends=True)
+        for chunk in chunks:
+            if chunk[-1] == '\n':
+                text += chunk[:-1]
+                ts = item['dt']
+                if ts == last_ts:
+                    ts = ''
+                lib.colors.print(template, time=ts, text=text)
+                last_ts = item['dt']
+                text = ''
+            else:
+                text += chunk
+    if text:
+        ts = item['dt']
+        if ts == last_ts:
+            ts = ''
+        lib.colors.print(template, time=ts, text=chunk)
 
 __all__ = ['main']
 
